a597ae8564215faf2d61c6fa8a74d4a15bca1375
Efanin <164139862+Efanin@users.noreply.github.com>
2025-06-12 17:53:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,57 @@
+import asyncio
+import logging
+import sys
+from os import getenv
+import os
+from aiogram import Bot, Dispatcher, html, Router, F
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+from functools import wraps
+import mode
 
+# Bot token can be obtained via https://t.me/BotFather
+bot = Bot("token")
+
+# All handlers should be attached to the Router (or Dispatcher)
+
+dp = Dispatcher()
+
+
+
+
+
+
+@dp.message(CommandStart()) 
+async def command_start_handler(message: Message) -> None:
+    await start_user(message)
+    await start_admin(message)
+
+
+
+@mode.user
+async def start_user(message: Message):
+    await message.answer(f"Hello user, {html.bold(message.from_user.full_name)}!")
+
+
+@mode.admin
+async def start_admin(message: Message):
+    await message.answer(f"Hello admin, {html.bold(message.from_user.full_name)}!")
+    
+
+
+@dp.message(Command("admin"))
+async def echo_handler(message: Message) -> None:
+    if True:
+        mode.state = "admin"
+        await message.answer("вы админ")
+
+
+async def main() -> None:
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
